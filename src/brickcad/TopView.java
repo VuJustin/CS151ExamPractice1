0b4b69f2136a4279e538cfdb3133c3387e156158
Justin Vu <justinhvu@gmail.com>
2023-03-22 16:32:56 -0700

    BrickCad added, now works

---
+++ src/brickcad/TopView.java
@@ -0,0 +1,24 @@
+package brickcad;
+
+import mvc.*;
+
+import java.awt.*;
+
+public class TopView extends View{
+    public TopView(Model brick) { super(brick); }
+
+    public void paintComponent(Graphics gc) {
+        super.paintComponent(gc);
+        Brick brick = (Brick)model;
+        Color oldColor = gc.getColor();
+        // Length and Width matter, but not height
+        double x = brick.getWidth();
+        double y = brick.getLength();
+        gc.setColor(Color.BLACK);
+        gc.drawString("Top View", 10, 10);
+        gc.setColor(Color.RED);
+        gc.fillRect(15, 15, (int)x, (int)y);
+
+        gc.setColor(oldColor);
+    }
+}
